4db15fb457ac71390852eb14fd03d0c646c9309f
Ammar1201 <aamour1201@gmail.com>
2022-12-15 17:22:57 +0200

    project initalize - server - changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 mongoose.set('strictQuery', false);
 
-const DB_URL = '';
+const DB_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.jbdlndj.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(DB_URL, (error, mongoDbInstance) => {
   if (err) {
